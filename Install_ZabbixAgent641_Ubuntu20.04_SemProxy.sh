86302c09b83ecb781fd1e69db8bf5e93d7cc83b1
Tiago Ribeiro <75284246+tialexandre@users.noreply.github.com>
2023-12-18 08:33:39 -0300

    Update Install_ZabbixAgent641_Ubuntu20.04_SemProxy.sh

--- Install_ZabbixAgent641_Ubuntu20.04_SemProxy.sh
+++ Install_ZabbixAgent641_Ubuntu20.04_SemProxy.sh
@@ -1,4 +1,4 @@
-#!/bin/bass 
+#!/bin/bash 
 wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu20.04_all.deb
 dpkg -i zabbix-release_6.4-1+ubuntu20.04_all.deb
 apt update -y
