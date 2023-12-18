0d45fdd6aa8ff83d4930a8909b4c82e30e4c20f3
Tiago Ribeiro <75284246+tialexandre@users.noreply.github.com>
2023-12-18 08:45:51 -0300

    Update Install_ZabbixAgent641_Ubuntu20.04_proxyDatacenterVivo.sh

--- Install_ZabbixAgent641_Ubuntu20.04_proxyDatacenterVivo.sh
+++ Install_ZabbixAgent641_Ubuntu20.04_proxyDatacenterVivo.sh
@@ -5,19 +5,17 @@ apt update -y
  
  apt install zabbix-agent -y 
  
- ARQUIVO -> /etc/zabbix/zabbix_agentd.conf
- 
  
 echo "LogFile=/var/log/zabbix/zabbix_agentd.log" > /etc/zabbix/zabbix_agentd.conf
 
-echo "LogFileSize=0" > /etc/zabbix/zabbix_agentd.conf
+echo "LogFileSize=0" >> /etc/zabbix/zabbix_agentd.conf
 
-echo "PidFile=/run/zabbix/zabbix_agentd.pid" > /etc/zabbix/zabbix_agentd.conf
+echo "PidFile=/run/zabbix/zabbix_agentd.pid" >> /etc/zabbix/zabbix_agentd.conf
 
-echo "Server=10.1.1.254,172.16.254.5" > /etc/zabbix/zabbix_agentd.conf
+echo "Server=10.1.1.254,172.16.254.5" >> /etc/zabbix/zabbix_agentd.conf
 
-echo "ServerActive=10.1.1.254" > /etc/zabbix/zabbix_agentd.conf
+echo "ServerActive=10.1.1.254" >> /etc/zabbix/zabbix_agentd.conf
 
-echo "Hostname=$HOSTNAME" > /etc/zabbix/zabbix_agentd.conf
+echo "Hostname=$HOSTNAME" >> /etc/zabbix/zabbix_agentd.conf
 
-echo "Include=/etc/zabbix/zabbix_agentd.d/*.conf" > /etc/zabbix/zabbix_agentd.conf
+echo "Include=/etc/zabbix/zabbix_agentd.d/*.conf" >> /etc/zabbix/zabbix_agentd.conf
