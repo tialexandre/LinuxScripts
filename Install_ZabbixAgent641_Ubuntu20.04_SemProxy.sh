aa528aea70363f008b5c365943c8e4b09276cbd9
Tiago Ribeiro <75284246+tialexandre@users.noreply.github.com>
2023-12-18 08:45:20 -0300

    Update Install_ZabbixAgent641_Ubuntu20.04_SemProxy.sh

--- Install_ZabbixAgent641_Ubuntu20.04_SemProxy.sh
+++ Install_ZabbixAgent641_Ubuntu20.04_SemProxy.sh
@@ -5,19 +5,19 @@ apt update -y
  
  apt install zabbix-agent -y 
  
- ARQUIVO -> /etc/zabbix/zabbix_agentd.conf
+
  
  
 echo "LogFile=/var/log/zabbix/zabbix_agentd.log" > /etc/zabbix/zabbix_agentd.conf
 
-echo "LogFileSize=0" > /etc/zabbix/zabbix_agentd.conf
+echo "LogFileSize=0" >> /etc/zabbix/zabbix_agentd.conf
 
-echo "PidFile=/run/zabbix/zabbix_agentd.pid" > /etc/zabbix/zabbix_agentd.conf
+echo "PidFile=/run/zabbix/zabbix_agentd.pid" >> /etc/zabbix/zabbix_agentd.conf
 
-echo "Server=10.3.1.102" > /etc/zabbix/zabbix_agentd.conf
+echo "Server=10.3.1.102" >> /etc/zabbix/zabbix_agentd.conf
 
-echo "ServerActive=10.3.1.102" > /etc/zabbix/zabbix_agentd.conf
+echo "ServerActive=10.3.1.102" >> /etc/zabbix/zabbix_agentd.conf
 
-echo "Hostname=$HOSTNAME" > /etc/zabbix/zabbix_agentd.conf
+echo "Hostname=$HOSTNAME" >> /etc/zabbix/zabbix_agentd.conf
 
-echo "Include=/etc/zabbix/zabbix_agentd.d/*.conf" > /etc/zabbix/zabbix_agentd.conf
+echo "Include=/etc/zabbix/zabbix_agentd.d/*.conf" >> /etc/zabbix/zabbix_agentd.conf
